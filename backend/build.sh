86849a3efbc03bb4963a088e99daa0fef9635e62
Yashi <yashiikpop@gmail.com>
2026-09-22 13:12:45 +0530

    feat : Seed production catalog

--- backend/build.sh
+++ backend/build.sh
@@ -4,3 +4,5 @@ set -o errexit
 python manage.py makemigrations
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
+python manage.py createsuperuser --noinput || true
+python manage.py seed_catalog
